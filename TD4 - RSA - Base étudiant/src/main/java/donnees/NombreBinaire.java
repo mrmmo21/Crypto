b8e6042999bcbab8cd0efb6f81ac521d8f0766ee
mrmmo21 <56302847+mrmmo21@users.noreply.github.com>
2020-03-19 17:05:00 +0100

    Update NombreBinaire.java

--- TD4 - RSA - Base étudiant/src/main/java/donnees/NombreBinaire.java
+++ TD4 - RSA - Base étudiant/src/main/java/donnees/NombreBinaire.java
@@ -275,7 +275,6 @@ public class NombreBinaire {
          NombreBinaire bPrime;
          int q = 0;
          while (!r.estInferieurA(b)){
-             System.out.println(r.estInferieurA(b));
             int n  = this.toString().length()-mot2.toString().length();
             bPrime = new NombreBinaire(b.decalage(n));
             if (!bPrime.estInferieurA(r)){
