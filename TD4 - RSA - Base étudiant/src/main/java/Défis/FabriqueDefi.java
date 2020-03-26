2f1009e0a59418c7c61f3c9ed0af03343670e110
Tinwen <tinwen.dev@gmail.com>
2020-03-26 16:30:15 +0100

    maj fabrique /code ?

--- TD4 - RSA - Base étudiant/src/main/java/Défis/FabriqueDefi.java
+++ TD4 - RSA - Base étudiant/src/main/java/Défis/FabriqueDefi.java
@@ -16,7 +16,7 @@ public class FabriqueDefi {
     
     public static Defis fabriqueDef(Client c, String str) throws Exception{
         Defis d;
-        System.out.println(str);
+        //System.out.println(str);
         switch(str){
             case "-- Début du défi : Connexion au serveur --" :
                 d = new Defi1();
@@ -30,19 +30,19 @@ public class FabriqueDefi {
             case "-- Début du défi : Soustraction --":
                 d = new Defi4();
             break;
-            case "-- Début du défi : Adiion --":
+            case "-- Début du défi : Est Inferieur --":
                 d = new Defi5();
             break;
-            case "-- Début du défi : Adition --":
+            case "-- Début du défi : Est Egal --":
                 d = new Defi6();
             break;
-            case "-- Début du éfi : Addition --":
+            case "-- Début du défi : Est Pair --":
                 d = new Defi7();
             break;
-            case "-- Début du déf : Addition --":
+            case "-- Début du défi : Multiplication --":
                 d = new Defi8();
             break;
-            case "-- Débt du défi : Addition --":
+            case "-- Début du défi : Quotient --": //JE SUIS LA MDR
                 d = new Defi9();
             break;
             case "-- Début du défi : Modulo --":
@@ -54,7 +54,7 @@ public class FabriqueDefi {
             case "-- Début du défi : Puissance Modulo --" :
                 d = new Defi12();
             break;
-            case "– Début du défi : Connon au serveur –" :
+            case "-- Début du défi : PGCD --" :
                 d = new Defi13();
             break;
             case "-- Début du défi : Random avec bornes --" :
@@ -90,9 +90,6 @@ public class FabriqueDefi {
             case "– Début du défi : Conexion au sereur –" :
                 d = new Defi24();
             break;
-            
-            
-            
             default:
                 throw new Exception("Erreur, defis non créé ou existant");
         }
