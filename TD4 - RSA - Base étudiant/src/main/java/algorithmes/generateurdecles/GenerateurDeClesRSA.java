85d846cb9578c4fe5b175d1b95fb34e106d5ca67
Viperbi <nickels.florian@gmail.com>
2020-03-26 16:18:08 +0100

    Update GenerateurDeClesRSA.java

--- TD4 - RSA - Base étudiant/src/main/java/algorithmes/generateurdecles/GenerateurDeClesRSA.java
+++ TD4 - RSA - Base étudiant/src/main/java/algorithmes/generateurdecles/GenerateurDeClesRSA.java
@@ -19,11 +19,34 @@ public class GenerateurDeClesRSA implements GenerateurDeCles{
     private NombreBinaire Q;
     private NombreBinaire N;;
     private NombreBinaire phi;
-    private NombreBinaire e;
-        
+        private NombreBinaire e;
+
+    public NombreBinaire getP() {
+        return P;
+    }
+
+    public NombreBinaire getQ() {
+        return Q;
+    }
+
+    public NombreBinaire getN() {
+        return N;
+    }
+
+    public NombreBinaire getPhi() {
+        return phi;
+    }
+
+    public NombreBinaire getE() {
+        return e;
+    }
+
     @Override
     public Cles genererClePublique() {
-       //TODO
+       NombreBinaire t = new NombreBinaire(1);
+       this.N = this.P.multiplication(this.Q);
+       this.phi = (this.P.soustraction(t)).multiplication(this.Q.soustraction(t));
+       
        return null;
     }
 
